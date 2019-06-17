d20d6f6dae2013569ec83c2ec1761f3dff0dae4f
MrGio7 <lashagiorgi1233@gmail.com>
2019-06-17 11:25:56 +0400

    added get .find function

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// implement your API here
+const express = require('express');
+const db = require('./data/db');
+
+const server = express();
+
+server.use(express.json());
+
+server.get('/', (req, res) => {
+    res.send('<h2>Server Up</h2>')
+});
+
+server.get('/api/users', (req, res) => {
+    db.find()
+    .then(allUsers => {
+        res.send(allUsers)
+    })
+    .catch( ({code, message}) => {
+        res.status(code).json({err: message})
+    })
+});
+
+
+
+server.listen(9090, () => {
+    console.log('listening port 9090')
+})
